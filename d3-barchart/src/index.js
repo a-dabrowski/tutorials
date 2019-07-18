ee299585618c2af372db68a6e87f7e063f81f0ad
Varyat <adam.dabrowski@outlook.com>
2019-07-18 08:58:25 +0200

    Init d3 bar chart tutorial

---
+++ d3-barchart/src/index.js
@@ -0,0 +1,23 @@
+import './index.css';
+import sample from './sample';
+const margin = 60;
+const width = 1000 - 2 * margin;
+const height = 600 - 2 * margin;
+
+const baseSvg = d3.select('svg');
+
+const chart = baseSvg.append('g').attr('transform', `translate(${margin}, ${margin})`);
+
+const yScale = d3.scaleLinear().range(height, 0).domain([0,100]);
+// height is first, because svg itself starts from top left corner,we want our chart to start from bottom left
+
+//append x axis
+chart.append('g').call(d3.axisLeft(yScale));
+
+const xScale = d3.bandScale().range([0, width])
+        .domain(sample.map(el => el.language))
+        .padding(0.2);
+
+chart.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale));
---
+++ d3-barchart/src/sample.js
@@ -0,0 +1,2 @@
+const sample = [];
+export default sample;
